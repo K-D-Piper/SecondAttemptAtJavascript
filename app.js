e251958a1f6c11ff7595226ca88019eb87f98f0c
K-D-Piper <kirsten.piper@mscoding.org>
2024-08-02 10:06:59 -0500

    Made HTML page. Made Notes in JS
    This is the start of my JS notes and my try at a second attempt to complete the projects of the week.

---
+++ app.js
@@ -0,0 +1,27 @@
+/* 
+
+// Welcome to my section of notes.
+JavaScript is a language to be layered on top of an HTML page to cause more fun elaborate effects. 
+
+In early sages; JavaScript's language is written in forumals that will then be added together and connected/linked to the your page through use of "console.log()"
+    // console.log () connects your coding page to webpage. At least at the start. 
+
+The statements work as followed/as written 
+
+"Statement + Declaration = Data Type " OR "Variable + variable name = data type "
+    How you write these is through use of RESERVED WORDS (also called methods) which are prevously defined words in JavaScript, bascailly so you and your computer know what you want/what you're trying to do. 
+        So in the end, how you'll basicilly write it is " Reserved Word + Variable Name = Data Type"
+
+* Just to further clarify text above; the addition sign is just to show that these items need to be connected. They are not needed/reccommneded during the actual act of making code. 
+
+// Reserved Words are the preset list of words that communicated the desired action of the variable. 
+// Declaration are the named YOU give the item that needs the desired effect. 
+// Data types are the only metrics that can be used. 
+
+//Data Types. 
+    Currently we are learning 5 data types. 
+        Strings : "Strings" / 'Strings' : Quation Words/ words that need to be indiviualed and defined
+        Numbers : 1- infinity : 
+        Booleans: True/False: 
+
+*/
