9371db20b504763191c150d191de0231ab107415
K-D-Piper <kirsten.piper@mscoding.org>
2024-08-13 16:08:26 -0500

    Few Grammatical Edits
    As rereading i made some edits. Deleted some spaces, added an * after data type to connect to explication. added word to understanding of Data types.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 /* 
-
 // Welcome to my section of notes.
 JavaScript is a language to be layered within an HTML page to cause more fun elaborate effects. 
 
@@ -8,13 +7,13 @@ The statements work as followed/as written
 
 "Statement + Declaration = Data Type " OR "Variable + variable name = data type "
     How you write these is through use of RESERVED WORDS (also called methods) which are prevously defined words in JavaScript, bascailly so you and your computer know what you want/what you're trying to do. 
-        So in the end, how you'll basicilly write it is " Reserved Word + Variable Name = Data Type"
+        So in the end, how you'll basicilly write it is " Reserved Word + Variable Name = Data Type"*
 
 * Just to further clarify text above; the addition sign is just to show that these items need to be connected. They are not needed/reccommneded during the actual act of making code. 
 
 // Reserved Words are the preset list of words that communicated the desired action of the variable. 
 // Declaration are the named YOU give the item that needs the desired effect. 
-// Data types are the only metrics that can be used. 
+// Data types are the only metrics that can be used/ measured
 
 //Data Types. 
     Currently we are learning 5 data types. 
