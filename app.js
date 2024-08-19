98b507363cc0f9f8b6be0df20e16aec636d75f85
K-D-Piper <kirsten.piper@mscoding.org>
2024-08-18 20:01:24 -0500

    Added Challenge Data. Began Active instruction for Day 9 (8/9/2024)
    Added challenge data to be plugged into coin roller. Added Day 9 Active Instruction (8/9/2024)

--- app.js
+++ app.js
@@ -1646,10 +1646,12 @@ So put them together.
                                                                     
 }  <--- End <------------------------------------------------------------|                           
 */ 
+
 // ACTIVE INSTRUCTION START DAY 7 (8/7/2024)
 //NESTED LOOPS
 //Global and BLOCKED scope 
 
+/* mistake when transcribing. Needs edits. 
 //Global = This variable is accessible from any point in this file.l 
 let global = "Hello!"
 //OUTER LOOP RUNS 10 TIMES
@@ -1661,6 +1663,7 @@ for (let i =1 ; i<= 10; i++){
     //    console.log(`${i} * ${j} = ${i *j} `)
     // }
     myName = "Margarito Valencia"; 
+
 }
 console.log(myName);
 
@@ -1673,7 +1676,7 @@ let firstWord = "Hello";
 }
 
 console.log(firstWord + secondWord)
-
+*/
 /*
 My understanding of global vs local / blocked scope...
 
@@ -1713,11 +1716,69 @@ After finished go to discussion tab and submit link to the Javascript Coin Proje
 // DAY 8 (8/8/2024) PROJECT/ASSIGNMENT END 
 
 // DAY 9 (8/9/2024) PROJECT/ ASSIGNMENT cont. START
+    //Challenge Data to test project. 
+var challengeData = ['dime','dime','dime','penny','nickel','penny','dime','quarter','dime','nickel','nickel','dime','quarter','nickel','penny','dime','nickel','dime','dime','dime','quarter','nickel','quarter','dime','nickel','penny','dime','penny','nickel','quarter','quarter','quarter','dime','nickel','dime','quarter','dime','penny','penny','nickel','penny','penny','dime','nickel','penny','penny','quarter','quarter','nickel','nickel','penny','dime','nickel','quarter','nickel','nickel','quarter','quarter','dime','nickel','penny','dime','dime','nickel','penny','quarter','quarter','quarter','dime','nickel','nickel','dime','quarter','quarter','nickel','penny','quarter','penny','quarter','nickel','dime','quarter','dime','penny','quarter','nickel','nickel','nickel','quarter','penny','nickel','nickel','dime','dime','dime','quarter','quarter','quarter','penny','penny','dime','dime','dime','penny','quarter','nickel','quarter','quarter','penny','nickel','penny','nickel','nickel','penny','quarter','nickel','dime','quarter','nickel','dime','penny','nickel','nickel','penny','penny','penny','dime','quarter','quarter','quarter','penny','dime','nickel','dime','dime','penny','quarter','penny','quarter','penny','dime','dime','nickel','dime','penny','penny','nickel','penny','nickel','quarter','dime','dime','nickel','nickel','nickel','dime','dime','dime','quarter','penny','dime','nickel','quarter','nickel','penny','nickel','penny','nickel','penny','nickel','nickel','penny','dime','nickel','dime','dime','quarter','quarter','quarter','penny','dime','penny','dime','dime','penny','penny','nickel','nickel','penny','quarter','nickel','quarter','dime','nickel','quarter','nickel','penny','dime','quarter','nickel','penny','quarter','quarter','penny','quarter','dime','penny','dime','nickel','penny','dime','quarter','nickel','quarter','quarter','quarter','dime','quarter','dime','quarter','nickel','penny','penny','quarter','quarter','penny','dime','penny','dime','penny','nickel','nickel','dime','penny','quarter','penny','quarter','nickel','quarter','penny','nickel','quarter','dime','quarter','dime','penny','dime','dime','quarter','penny','penny','nickel','nickel','quarter','nickel','quarter','dime','quarter','penny','penny','penny','nickel','nickel','quarter','penny','quarter','quarter','penny','nickel','dime','penny','quarter','nickel','dime','penny','penny','quarter','penny','penny','dime','nickel','dime','penny','dime','nickel','nickel','dime','dime','penny','nickel','penny','dime','dime','nickel','dime','dime','penny','penny','dime','penny','nickel','quarter','nickel','quarter','quarter','penny','nickel','penny','quarter','nickel','nickel','quarter','penny','penny','nickel','nickel','penny','penny','nickel','quarter','nickel','dime','nickel','nickel','penny','quarter','dime','dime','nickel','dime','nickel','nickel','dime','quarter','quarter','nickel','penny','dime','dime','dime','dime','quarter','nickel','nickel','dime','nickel','quarter','nickel','dime','quarter','nickel','dime','quarter','quarter','penny','quarter','nickel','dime','quarter','penny','penny','nickel','nickel','quarter','dime','penny','dime','dime','quarter','dime','penny','quarter','penny','penny','nickel','nickel','nickel','penny','penny','quarter','penny','penny','quarter','dime','quarter','nickel','quarter','nickel','penny','penny','penny','dime','dime','dime','dime','nickel','dime','quarter','nickel','dime','nickel','dime','dime','penny','dime','quarter','nickel','penny','nickel','penny','penny','quarter','dime','nickel','nickel','nickel','penny','nickel','dime','penny','quarter','quarter','dime','quarter','penny','nickel','nickel','penny','dime','dime','nickel','nickel','dime','penny','nickel','penny','quarter','dime','nickel','dime','penny','penny','quarter','dime','quarter','penny','penny','nickel','quarter','quarter','quarter','dime','penny','dime','dime','dime','quarter','penny','penny','dime','quarter','dime','penny','quarter','dime','dime','dime']
+
+//DAY 9 (8/9/2024) ACTIVE INSTRUCIION/ PROJECT REVIEW START
+
+console.log(challengeData.length)
+    //!
+    //? Give me the total amount of each coin;
+    // ex. dime = 234;
+    //? Convert the total amount of coins into dollars (USD currency)
+    // ex. dimeCurrencyTotal = $20.20
+    //? Total amount Currency all together (PenniesCurrencyTotal + QuartersCurrencyTotal... .etc)
+
+    //Creating this variable to be able to store information; 
+    let totalPennyCount = 0;
+    let totalDimeCount = 0;
+    let totalNickelCount = 0; 
+    let totalQuarterCount = 0;
+
+    let pennyValue = .01;
+    let dimeValue = .10;
+    let nickelValiue = .05;
+    let quarterValue = .25;
+
+    for (let i =0; i < challengeData.length; i++){
+        //console.log(i)
+        //how am i going to pick out all the pennies?
+        //Based on condion = if statments 
+        //console.log(challengeData[i])
+        if (challengeData[i] === 'penny') {
+            totalPennyCount++
+
+        } else if (challengeData[i] === 'dime'){
+            totalDimeCount += 1;
+        } else if (challengeData[i] === 'nickel'){
+            totalNickelCount += 1;
+        } else if (challengeData[i] === 'quarter') {
+            totalQuarterCount += 1;
+        }
+}
+console.log(totalPennyCount)
+console.log(totalDimeCount)
+console.log(totalNickelCount)
+console.log(totalQuarterCount)
+
+let pennyConversion = totalPennyCount * pennyValue;
+let dimeConversion = totalDimeCount * dimeValue
+let nickelConversion = totalNickelCount * nickelValue;
+let quarterConversion = totalQuarterCount * quarterValue;
+
+console.log('$' + pennyConversion)
+console.log('$' + dimeConversion.toFixed(2));
+console.log('$' + nickelConversion.toFixed(2));
+console.log('$' + quarterConversion.toFixed(2));
+console.log(pennyConversion + dimeConversion + nickelConversion + quarterConversion )
+
+//DAY 9 (8/9/2024) ACTIVE INSTRUCITON/ PROJECT REVIEW END 
+//DAY 9 (8/9/2024) ACTIVE INSTRUCTION/ FUNCTIONS START
 
 
 
-
-
+// DAY 9 (8/9/2024) ACTIVE INSTRUCTION END
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 // MY QUESTIONS 
 /*
 //So, if the ! means not and puting in front of a true makes it false, why is that sometimes a better case than just writing false?
