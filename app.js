fcfae2aea3b986783ec4bfba2c928bdc387afdd6
K-D-Piper <kirsten.piper@mscoding.org>
2024-09-11 19:16:56 -0500

    Added Day 12 (8/14/2024) Active Instruction
    Added Day 12 (8/14/2024) Active instruction and quiz problems

--- app.js
+++ app.js
@@ -2282,8 +2282,186 @@ let balanceChecker = bankChecker(bankBalance);
 
 //Christopher's Bank Account Manger END 
 // DAY 11 (8/13/2024) ACTIVE INSTRUCTION & QUIZ PROBLEM REIVEWS END 
+//DAY 12 (8/14/2024) ACTIVE INSTRUCTION START
+/*
+    Introduction to Objects. 
+    Objects are like more detalied arrays. They hold multiple data sets.
+    Written like this...
+    
+    var obj = {
+        property: value,
+        key: value,
+        propety:value
+    }
+
+    objects allow you to access data within an array that holds miltiple peices of information. 
+    exp.
+    
+    var person = {
+    fName: 'Dwayne',
+    lName:'Johnson',
+    alias:'The Rock',
+    "occupation": 'President of the United States',
+    "other jobs": ['wrestler', 'actor'],
+    age: 51,
+    isStrong: true
+    }
+
+    but what if one needs to call a property? 
+    to access a propety one should call a property name or variable
+    exp. Access "The Rock"
+
+    synax : 
+        obj.key
+
+    so for the example it would look like 
+        1. Dot Notation 
+            person.alias => 'The Rock'
+        2. Bracket Notation 
+            obj["The Rock"]
+
+    you can also use bracket notation to call several things as well. 
+        person["other jobs"] => ['wrestler', 'actor']
+        or
+        person['other jobs'][1]=> 'actor'
+*/
+
+//! Objects
+
+//Comparable to arrays just more detailed - short summary - michael 
+
+//syntax for objects 
+let favoriteCharacter = {
+    fName: "Bob",
+    lName: "Ross",
+    hobbies:["painting", "Animal Caring"],
+    occupation:["Drill Sargent", "TV Peronality"],
+    age: 52,
+    CurrentHealth: "Dead",
+    catchphrase: quoter(),
+    catchPhrase: function(catchPhrase){
+        console.log(catchPhrase)
+    }
+};
+//This calls the entire object data
+console.log(favoriteCharacter)
+//Callinig one of the property values;
+console.log(`My favorite charcter's first name is ${favoriteCharacter.fName}`)
+//Calling a propety that has a ARRAY data type
+console.log(`One of my favorite character's hobbies is ${favoriteCharacter.hobbies[1]}`)
+
+//This will update a propety value 
+favoriteCharacter.currentHealth = "Alive and well in spirit!";
+
+console.log(favoriteCharacter.currentHealth);
+
+//This will add a new propety to the object  
+favoriteCharacter.PaintingSkills = "HE WAS SO GOOODOOOD";
+
+console.log(favoriteCharacter)
+
+/*
+ Objects can also be connected to functions. 
+    exp. 
+    person.quote = function () {
+    var message = 'Can you SMELL what The Rock...is cooking??';
+    console.log(message);
+    return message;
+    }
+
+    however it would be only stored to the propety value. but it would probaby be better to set up your functions differently. 
+*/
 
 
+//Objects can store funcitons
+
+function quoter(){
+    let quote = "There are no mistakes only happy accidents."
+    return quote
+}
+
+favoriteCharacter.catchPhrase("There are no mistakes only happy accidents.")
+
+//! Arrays of Objects
+
+let listOfMusic = [
+    {
+        title: 'Never gonna give you up',
+        artist: 'Rick Astley',
+        year: 1987,
+    },
+    {
+        title:'Black Magic Woman',
+        artist: 'Santana',
+        year: 1960
+    }
+]
+
+//Format nameOfVar[#] = the whole object in that index 
+console.log(`${listOfMusic[0]} will grab the whole object in that index number`);
+
+//Gives you teh lenth of propeties in a object 
+let currentObjectLength = Object.keys(listOfMusic[0].length).length;
+console.log(`${currentObjectLenth} is the number of propeties`);
+
+//This will return object object. Specify whatcha need from this object!
+console.log(`${listOfMusic[1]} will return object object in a template literal`);
+
+//DAY 12 (8/14/2024) ACTIVE INSTRUCTION END
+
+//QUIZ PROBLEMS DAY 12 (8/14/2024) START
+
+//(1)
+//! Personal Contact Book
+//? Create a simple contact book where you can add, view, and search for contacts by name.
+ 
+//TODO Define a Contact Object: Each contact should be represented as an object with properties like name, phone number, and email.
+//TODO Store Contacts in an Array: Use an array to store multiple contact objects.
+//TODO Create Functions: Craeate functions to add a contact, view all contacts, and search for a contact by name.
+//* Output Results: Use console.log to display the contacts and search results.
+ 
+//(2)
+//! Student Grade Management
+//? Create a program to manage student grades, where you can calculate the average grade for the class and identify students who need to improve.
+ 
+//TODO Predefine Student Objects: Each student should be an object with properties like name and grade, stored inside an array.
+let students = [
+    { name: "Alice", grade: 85 },
+    { name: "Bob", grade: 76 },
+    { name: "Charlie", grade: 92 },
+    { name: "David", grade: 63 },
+    { name: "Eve", grade: 78 }
+  ];
+ 
+//TODO Calculate Average Grade: Write a function to calculate the average grade of the class.
+//TODO Identify Students Below Average: Write a function to find and list students with grades below the class average.
+ 
+ 
+//(3)
+//! Movie Collection Tracker
+//? Create a program that allows you to manage a collection of movies, including adding movies, rating them, and viewing your collection.
+ 
+//TODO Define a Movie Object: Each movie should be represented as an object with properties like title, genre, and rating.
+//TODO Store Movies in an Array: Use an array to store multiple movie objects.
+//TODO Create Functions: Create functions to add a movie, rate a movie, and view your entire collection.
+//* Output Results: Use console.log to display the movie collection and ratings.
+ 
+//(4)
+//! Library Book Tracker
+//? Create a program to manage a small library, where you can check which books are available or borrowed, and filter the books by their status.
+ 
+//TODO Predefine Book Objects: Each book should be an object with properties like title and status (available or borrowed), stored inside an array.
+let books = [
+    { title: "The Great Gatsby", status: "available" },
+    { title: "1984", status: "borrowed" },
+    { title: "To Kill a Mockingbird", status: "available" },
+    { title: "The Catcher in the Rye", status: "borrowed" },
+    { title: "Moby-Dick", status: "available" }
+  ];
+//TODO Filter Available Books: Write a function to list all available books.
+//TODO Filter Borrowed Books: Write a function to list all borrowed books.
+
+//QUIZ PROBLEMS DAY 12 (8/14/2024) END
 
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
